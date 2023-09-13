ff0a1dd666a0f113bef405ae6f61b97ade7cf823
Pedro-Henrique-da-Silva-Ferreira <141925740+Pedro-Henrique-da-Silva-Ferreira@users.noreply.github.com>
2023-09-13 11:39:51 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
@@ -13,22 +14,49 @@ public class Main {
 
 
         Scanner sc = new Scanner(System.in);
+        Repositorio2d repositorio2d = new Repositorio2d();
+
         System.out.println(menu);
         int opcao = sc.nextInt();
 
-        switch (opcao){
+        switch (opcao) {
             case 1:
+                System.out.println("É um quadrado ou um circulo?");
+                char resFigura = sc.next().charAt(0);
+
+                if (resFigura == 'q') {
+                    System.out.println("Digite o numero do lado do quadrado:");
+                    double lado = sc.nextDouble();
+                    Figura figura = new Quadrado(lado);
+
+                    System.out.println("Area do quadrado");
+                    System.out.println(figura.area());
+
+                    System.out.println("Perimetro do quadrado");
+                    System.out.println(figura.perimetro());
+                    repositorio2d.adicionarFigura(figura);
+
+                } else if (resFigura == 'c') {
+                    System.out.println("Digite o numero do raio do circulo:");
+                    double raio = sc.nextDouble();
+                    Figura figura = new Circulo(raio);
+
+                    System.out.println("Area do circulo");
+                    System.out.printf("%.2f" , figura.area());
+
+                    System.out.println("\nPerimetro do circulo");
+                    System.out.printf("%.2f" , figura.perimetro());
+                    repositorio2d.adicionarFigura(figura);
+                }
+                break;
+
+            case 2:
+                System.out.println("Informe o numero");
+                int remover = sc.nextInt();
+
+
+
         }
-
-        System.out.println("Digite um numero:");
-        double lado = sc.nextDouble();
-        Figura figura = new Quadrado(lado);
-
-        System.out.println("Area do quadrado");
-        System.out.println(figura.area());
-
-        System.out.println("Perimetro");
-        System.out.println(figura.perimetro());
 
 
     }
